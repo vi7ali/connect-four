c78c1578a8545875f101c400e625c0c8aa7d5e04
Vitaly Osipov <vi7ali@gmail.com>
2019-03-29 15:55:29 +0300

    Add is_valid_move? method and the test for it

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,4 +1,7 @@
 class Board
+
+  ROWS = 6
+  COLUMNS = 7
   attr_accessor :grid
   
   def initialize
@@ -6,16 +9,37 @@ class Board
   end
 
   def generate_grid
-    grid = []
-    6.times do
+    self.grid = []
+
+    ROWS.times do
       row = []
-      7.times do        
+      COLUMNS.times do        
         row.push(" ")
       end
-      grid.push(row)
+      self.grid.push(row)
     end
+
     grid
   end
-
   
+  def update_grid(column,color)
+    if is_valid_move?(column)
+      self.grid[get_row][column] = color
+      grid
+    end
+  end
+
+  def is_valid_move?(c)
+    r = 0
+    while(r < ROWS)
+      return true if grid[r][c] == " "
+      r += 1
+    end
+    return false
+  end
+
+  def get_row
+    return 2
+  end
+
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -17,7 +17,17 @@ RSpec.describe "Connect Four" do
     end
 
     it "generates the grid of 7 columns and 6 rows"do
-      expect(board.generate_grid).to eql(grid)
+      expect(board.grid).to eql(grid)
+    end
+
+    it "updates the grid" do
+      grid[2][6] = "r"
+      expect(board.update_grid(6,"r")).to eql(grid)
+    end
+
+    it "checks for valid move" do
+      6.times {|index| board.grid[index][0] = "r"}
+      expect(board.is_valid_move?(0)).to eql(false)
     end
   end
 end
